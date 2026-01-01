098c8afa6f8dbe5419c954f57770823cab433c3c
Riley Pinkham <riley.pinkham@DC-CT79F7MXCT.local>
2025-12-31 17:23:41 -0700

    .

--- src/shared/CommandModels.cpp
+++ src/shared/CommandModels.cpp
@@ -2,3 +2,25 @@
 #include "CommandModels.h"
 
 
+
+CreateTaskCommand::CreateTaskCommand(){
+    
+}
+
+class CreateTaskCommand : public Command {
+public:
+    std::string title;
+    std::string description;
+    Task::Priority priority; // Using the Task model's enum
+    std::string assignee;
+    
+    CreateTaskCommand() : Command(CommandType::CREATE_TASK) {};
+};
+
+class GetTaskCommand : public Command {
+public:
+    int id;
+    
+    GetTaskCommand() : Command(CommandType::GET_TASK) {};
+};
+
